3e15d3cc54278c35cc6615e475fa26466eccf8b6
pedrosamegima <136722484+pedrosamegima@users.noreply.github.com>
2024-04-25 16:28:32 -0300

    Add files via upload

---
+++ MapRelacioTurma/src/main/java/com/MapRelacioTurma/MapRelacioTurmaApplication.java
@@ -0,0 +1,13 @@
+package com.MapRelacioTurma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapRelacioTurmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapRelacioTurmaApplication.class, args);
+	}
+
+}
